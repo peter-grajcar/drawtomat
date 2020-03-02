3be641af93b7aa94f55b78530107eaf0f870b28a
petergrajcar <peter.grajcar131@gmail.com>
2020-03-02 18:21:30 +0100

    Edit docs config

--- drawtomat/docs/source/conf.py
+++ drawtomat/docs/source/conf.py
@@ -29,7 +29,7 @@ author = 'Peter Grajcar'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
+    "numpydoc"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
