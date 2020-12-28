00dae48dab89528636f6bd21245fd399212f95fd
Peter Grajcar <peter.grajcar131@gmail.com>
2020-12-28 17:14:32 +0100

    improve script for generating relative sizes
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- experiments/scenegraph-sizes/sizes_relative.py
+++ experiments/scenegraph-sizes/sizes_relative.py
@@ -7,50 +7,73 @@ import json
 from collections import defaultdict
 from fasttext_embedding import Embedding
 
+use_word_embedding = False
+
+# load word embedding model
 embedding = Embedding("../word2vec/conceptual-captions-fasttext.model")
-similar_words = {}
+similar_words = {} # cache for the most similar words
+threshold = 0.85
+
+# list of quickdraw categories
+quickdraw = [category[0] for category in embedding.categories]
 
 sizes = defaultdict(lambda: { "count": 0, "sum_w": 0, "sum_h": 0 })
 
-threshold = 0.85
 counter = 0
-
-for line in sys.stdin:
-    counter += 1
-    if counter % 100 == 99:
-        print(counter + 1, file=sys.stderr, end="\r")
-    
-    data = json.loads(line)
-    pred = data["predicate"].upper().replace(" ", "_")
-    
-    sub_name = data["subject"]["name"]
-    obj_name = data["object"]["name"]
-
-    if sub_name in similar_words:
-        sub, sim = similar_words[sub_name]
-        if sim < threshold:
-            continue
-    else:
-        sub, sim = embedding.most_similar(sub_name, include_similarity=True)
-        similar_words[sub_name] = (sub, sim)
-        if sim < threshold:
-            continue
-
-    if obj_name in similar_words:
-        obj, sim = similar_words[obj_name]
-        if sim < threshold:
-            continue
-    else:
-        obj, sim = embedding.most_similar(obj_name, include_similarity=True)
-        similar_words[obj_name] = (obj, sim)
-        if sim < threshold:
-            continue
+if not use_word_embedding:
+    # without word embedding
+    for line in sys.stdin:
+        counter += 1
+        if counter % 100 == 99:
+            print(counter + 1, file=sys.stderr, end="\r")
         
-    key = f"{sub},{obj}"
-    sizes[key]["count"] += 1
-    sizes[key]["sum_w"] += data["subject"]["w"] / data["object"]["w"]
-    sizes[key]["sum_h"] += data["subject"]["h"] / data["object"]["h"]
-    # print(f"{pred}({sub},{obj})")
+        data = json.loads(line)
+        pred = data["predicate"].upper().replace(" ", "_")
+        sub = data["subject"]["name"]
+        obj = data["object"]["name"]
+        if sub in quickdraw and obj in quickdraw:
+            key = f"{sub},{obj}"
+            sizes[key]["count"] += 1
+            sizes[key]["sum_w"] += data["subject"]["w"] / data["object"]["w"]
+            sizes[key]["sum_h"] += data["subject"]["h"] / data["object"]["h"]
+else:
+    # with word embedding
+    for line in sys.stdin:
+        counter += 1
+        if counter % 100 == 99:
+            print(counter + 1, file=sys.stderr, end="\r")
+        
+        data = json.loads(line)
+        pred = data["predicate"].upper().replace(" ", "_")
+        
+        sub_name = data["subject"]["name"]
+        obj_name = data["object"]["name"]
+
+        if sub_name in similar_words:
+            sub, sim = similar_words[sub_name]
+            if sim < threshold:
+                continue
+        else:
+            sub, sim = embedding.most_similar(sub_name, include_similarity=True)
+            similar_words[sub_name] = (sub, sim)
+            if sim < threshold:
+                continue
+
+        if obj_name in similar_words:
+            obj, sim = similar_words[obj_name]
+            if sim < threshold:
+                continue
+        else:
+            obj, sim = embedding.most_similar(obj_name, include_similarity=True)
+            similar_words[obj_name] = (obj, sim)
+            if sim < threshold:
+                continue
+            
+        key = f"{sub},{obj}"
+        sizes[key]["count"] += 1
+        sizes[key]["sum_w"] += data["subject"]["w"] / data["object"]["w"]
+        sizes[key]["sum_h"] += data["subject"]["h"] / data["object"]["h"]
+        # print(f"{pred}({sub},{obj})")
 
 
 for key, value in sizes.items():
@@ -59,6 +82,6 @@ for key, value in sizes.items():
     h = value["sum_h"] / value["count"]
     sub, obj = key.split(",")
     if not f"{obj},{sub}" in sizes:
-        print(f"{obj},{sub},{1/w},{1/h}")
-    print(f"{sub},{obj},{w},{h}")
+        print(obj, sub, 1/w, 1/h, sep=",")
+    print(sub, obj, w, h, sep=",")
 
