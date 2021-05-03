893e0e3f003eb0acaa7b7e39884bfc7cc79b049a
Peter Grajcar <peter.grajcar131@gmail.com>
2021-05-03 19:40:50 +0200

    add 'sizes' and 'constraints' options to the CLI
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- drawtomat/setup.py
+++ drawtomat/setup.py
@@ -5,4 +5,11 @@ setup(
     description="Automatic generation of drawings from description",
     version="0.0.1",
     author="Peter Grajcar",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    data_files=[("resources", [
+        "resources/predicates.txt",
+        "resources/logging.conf",
+        "resources/quickdraw",
+    ])]
 )
--- drawtomat/src/drawtomat/__main__.py
+++ drawtomat/src/drawtomat/__main__.py
@@ -3,7 +3,7 @@ import logging.config
 
 from drawtomat.composer import ConstraintComposer
 from drawtomat.composer.factory.quickdraw_object_factory import QuickDrawObjectFactory
-from drawtomat.composer.scaler.absolute_object_scaler import AbsoluteObjectScaler
+from drawtomat.composer.scaler import AbsoluteObjectScaler, RelativeObjectScaler
 from drawtomat.processor.udpipe_processor import UDPipeProcessor
 # A dog and a chair are inside a house. The dog is sitting on the chair.
 from drawtomat.processor.word_embedding import WordEmbedding
@@ -17,6 +17,8 @@ if __name__ == "__main__":
     parser.add_argument("--description", type=str, help="Specifies the image description. If not set the description "
                                                         "is taken from the standard input.")
     parser.add_argument("--graph_output", type=str, help="Scene graph output path.")
+    parser.add_argument("--sizes", type=str, choices=["absolute", "relative"], default="absolute", help="Approach for determining the object size. Default is 'absolute'.")
+    parser.add_argument("--constraints", type=str, choices=["rule", "classifier"], default="rule", help="Type of the constraints. Default is 'rule'.")
     parser.add_argument("--image_output", default="drawing.png", type=str, help="Image output path. Default is "
                                                                                 "drawing.png")
     parser.add_argument("--show", action="store_true", default="Show the image after it is generated.")
@@ -36,9 +38,15 @@ if __name__ == "__main__":
 
     word_embedding = WordEmbedding(QuickDrawDataset.words())
     obj_factory = QuickDrawObjectFactory(word_embedding)
-    obj_scaler = AbsoluteObjectScaler(word_embedding)
-    composer = ConstraintComposer(obj_factory, obj_scaler)
-    entities = composer.compose(scene)
 
+    if args.sizes == "absolute":
+        obj_scaler = AbsoluteObjectScaler(word_embedding)
+    elif args.sizes == "relative":
+        obj_scaler = RelativeObjectScaler(word_embedding)
+    else:
+        raise ValueError(f"Invalid scaler type '{args.sizes}'.")
+
+    composer = ConstraintComposer(obj_factory, obj_scaler, constraints=args.constraints)
+    entities = composer.compose(scene)
     renderer = SimpleRenderer(composer=composer, show_bounds=False)
     renderer.render(scene, show=args.show, output=args.image_output)
